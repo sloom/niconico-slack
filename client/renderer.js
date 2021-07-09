77dd29ca6dfe1d0846d79e41dc59fa0f8ed96f20
sloom <owabichan@gmail.com>
2021-07-09 21:50:24 +0900

    - Support emoji.

--- client/renderer.js
+++ client/renderer.js
@@ -2,8 +2,19 @@
 const { ipcRenderer } = require('electron');
 const NicommentJS = require('./lib/nicommentJS.js');
 const io = require('./lib/socket.io-2.1.1.min.js');
+const emoji = require('./resource/slack_emoji.json');
+ 
 
 document.addEventListener('DOMContentLoaded', () => {
+    // 絵文字検出用RegExpオブジェクトの作成
+    const emoji = require('./resource/slack_emoji.json');
+    const emojiRegExps = {};
+    for (var key in emoji) {
+        const newRegExp = new RegExp(String.raw`:${key}:`, 'g');
+        emojiRegExps[key] = newRegExp;
+    }
+    const emojiColumn = /:/g
+
     const hosts = ipcRenderer.sendSync('get-host-config');
     const size = ipcRenderer.sendSync('get-primary-display-size');
     var nico = new NicommentJS({
@@ -19,6 +30,18 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('message receive: ' + msg);
         if (!msg) { return; } // msgがnullの時があるので
         msg = msg.replace(/<@.*>/, '');
+        const columnCount = (msg.match(emojiColumn) || []).length;
+        if (columnCount >= 2) { // コロン2つが含まれた場合、絵文字を検索
+            for (emojiRegExpKey in emojiRegExps) {
+                const emojiRegExp = emojiRegExps[emojiRegExpKey];
+                if (msg.match(emojiRegExp)) {
+                    const entity = emoji[emojiRegExpKey];
+                    msg = msg.replace(emojiRegExp, entity);
+                    // break すると複数の絵文字に変換できないのでしない
+                }
+            }
+        }
+
         nico.send(msg);
     });
 });
